e097da008737e171d42c3972ff297d0a2bf62d17
LeoFdz <lfernandez79@gmail.com>
2020-03-25 10:26:42 -0500

    init structure

---
+++ client/src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import {  Router as Route, Switch } from "react-router-dom";
+import Books from "./pages/Books";
+import Detail from "./pages/Detail";
+import NoMatch from "./pages/NoMatch";
+import Nav from "./components/Nav";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path={["/", "/books"]}>
+            <Books />
+          </Route>
+          <Route exact path="/books/:id">
+            <Detail />
+          </Route>
+          <Route>
+            <NoMatch />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
